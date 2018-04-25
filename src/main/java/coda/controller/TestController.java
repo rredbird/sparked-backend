844dabe6df936dccc7c4d16f2e9fe127edfa8a73
Robin Ruth <robin.ruth.tub@gmail.com>
2018-04-25 16:03:46 +0200

    Allow Cross-Site-Origin-Requests from localhost:4200 (Angular)

--- src/main/java/coda/controller/TestController.java
+++ src/main/java/coda/controller/TestController.java
@@ -1,12 +1,14 @@
 package coda.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class TestController {
-    @RequestMapping("/greeting")
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return "Hello Coda";
     }
