619761d61c1cb4aab65403be7bb8b26bf8556869
Robin Ruth <robin.ruth.tub@gmail.com>
2018-04-25 15:05:06 +0200

    new file:   .gitignore 	deleted:    .gradle/4.7/fileChanges/last-build.bin 	deleted:    .gradle/4.7/fileContent/annotation-processors.bin deleted:    .gradle/4.7/fileContent/fileContent.lock 	deleted:    .gradle/4.7/fileHashes/fileHashes.bin 	deleted:    .gradle/4.7/fileHashes/fileHashes.lock 	deleted:    .gradle/4.7/fileHashes/resourceHashesCache.bin 	deleted:    .gradle/4.7/taskHistory/taskHistory.bin deleted:    .gradle/4.7/taskHistory/taskHistory.lock 	deleted:    .gradle/buildOutputCleanup/buildOutputCleanup.lock 	deleted:    .gradle/buildOutputCleanup/cache.properties 	deleted:    .gradle/buildOutputCleanup/outputFiles.bin 	deleted:    .gradle/vcsWorkingDirs/gc.properties 	deleted:    build/classes/java/main/main/java/coda/Application.class 	deleted:    build/classes/java/main/main/java/coda/controller/TestController.class 	deleted:    build/libs/coda-webapi-0.1.0.jar 	deleted:    build/tmp/bootJar/MANIFEST.MF deleted:    gradle/wrapper/gradle-wrapper.jar 	deleted:    gradle/wrapper/gradle-wrapper.properties 	deleted:    gradlew 	deleted:    gradlew.bat 	modified:   src/main/java/coda/Application.java 	modified:   src/main/java/coda/controller/TestController.java

--- src/main/java/coda/Application.java
+++ src/main/java/coda/Application.java
@@ -1,4 +1,4 @@
-package main.java.coda;
+package coda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/coda/controller/TestController.java
+++ src/main/java/coda/controller/TestController.java
@@ -1,6 +1,5 @@
-package main.java.coda.controller;
+package coda.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
