2a16c7d352dc6e622c3a3eed8ff0777136d70f90
Robin Ruth <robin.ruth.tub@gmail.com>
2018-04-25 16:49:13 +0200

    changed greeting interface to return typed Greeting class, which is automatically translated to json, because angular expects a json response.

--- src/main/java/coda/controller/TestController.java
+++ src/main/java/coda/controller/TestController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import coda.shared.dto.Greeting;
+
 @RestController
 public class TestController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello Coda";
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(1, "Hello Coda");
     }
 }
