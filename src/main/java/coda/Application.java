cfa3ef4e800a10b5942a893564677672d58bb964
beuthdev\rui <robin.ruth@beuth.de>
2018-05-24 18:44:21 +0200

    config.properties file and writing properties to that file.

--- src/main/java/coda/Application.java
+++ src/main/java/coda/Application.java
@@ -1,14 +1,50 @@
 package coda;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    
+    private void startApplication(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private void writePropertiesFile() {
+        OutputStream propertiesOutputStream = null;
+        Properties properties = new Properties();
+        
+        properties.setProperty("mongo_Port", "27017");
+
+        try {
+            propertiesOutputStream = new FileOutputStream("config.properties");
+            properties.store(propertiesOutputStream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                propertiesOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }                
+        }
+    }
+
     /**
      * Program start function. Starts the Spring application
      */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        Application application = new Application();
+
+        application.startApplication(args);
+
+        application.writePropertiesFile();
+
     }
 }
