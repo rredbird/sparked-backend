6db6094ab4055142c19e66dbf472bf53fae443d1
Robin Ruth <robin.ruth.tub@gmail.com>
2018-06-10 11:19:57 +0200

    file upload working

--- src/main/java/coda/controller/TestController.java
+++ src/main/java/coda/controller/TestController.java
@@ -17,8 +17,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -53,7 +53,7 @@ public class TestController {
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/upload")
+    @PostMapping("/upload")
     public void upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
         log.debug(file.getOriginalFilename());
     }
