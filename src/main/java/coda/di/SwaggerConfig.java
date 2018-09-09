794fc603ca96d414296deb5fc002f7e241010bac
Robin Ruth <robin.ruth.tub@gmail.com>
2018-09-09 15:53:31 +0200

    merge errors

--- src/main/java/coda/di/SwaggerConfig.java
+++ src/main/java/coda/di/SwaggerConfig.java
@@ -1,17 +1,10 @@
 package coda.di;
 
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import coda.controller.OrderController;
-=======
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
->>>>>>> 92b37bd6a6d5375b71b98df2ff545ee90fbbe972
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -20,19 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-<<<<<<< HEAD
-@ComponentScan(basePackageClasses = {
-    OrderController.class
-})
-public class SwaggerConfig {                                    
-    @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-=======
 public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket productApi() {
@@ -50,6 +30,5 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
  
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
->>>>>>> 92b37bd6a6d5375b71b98df2ff545ee90fbbe972
     }
 }
--- src/main/java/coda/shared/logging/Logging.java
+++ src/main/java/coda/shared/logging/Logging.java
@@ -1,27 +1,8 @@
 package coda.shared.logging;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-
-import org.bson.Document;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-@Component("logging")
-public class Logging {
-    private static Logger logger = LogManager.getLogger(Logging.class);
-=======
 public class Logging implements ILogging {
->>>>>>> 92b37bd6a6d5375b71b98df2ff545ee90fbbe972
 
     public Logging() {
-        System.out.println("Logging constructor");
-
-        logger.error("Log4j test");
     }
 
     @Override
